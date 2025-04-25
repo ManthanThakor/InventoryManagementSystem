5b12b0713b75c97fae047131fb174c9806c67e9a
manthan <thakormanthan849@gmail.com>
2025-04-25 12:51:03 +0530

    change

--- InventoryManagementSystem/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ InventoryManagementSystem/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21f52b77bfda40af843f339e62df81c904fd76a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed3ea8a09e9af8dfbe7d2551e86b111b459eba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
