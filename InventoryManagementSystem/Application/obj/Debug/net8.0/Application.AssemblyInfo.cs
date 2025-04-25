d7135b6c029972ce4ab38f8e7a28244822b0e28d
manthan <thakormanthan849@gmail.com>
2025-04-25 11:19:14 +0530

    change

--- InventoryManagementSystem/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ InventoryManagementSystem/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248a6df99ebbc24e4fdc1bd28a21f7fda054ac7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fca466443a153f6ad0fe3510d7d97e9ffc04a276")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
