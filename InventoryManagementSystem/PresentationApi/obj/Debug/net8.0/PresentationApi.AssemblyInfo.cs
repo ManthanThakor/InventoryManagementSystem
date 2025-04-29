802f55dcc09971edcc765d4b7ade5375c6b55232
manthan <thakormanthan849@gmail.com>
2025-04-29 17:13:05 +0530

    change

--- InventoryManagementSystem/PresentationApi/obj/Debug/net8.0/PresentationApi.AssemblyInfo.cs
+++ InventoryManagementSystem/PresentationApi/obj/Debug/net8.0/PresentationApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10ec2cffe6c125f3baa17cdc450d2b7e00655a9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89ccbb2b9d44bd7ba7317920fabf3267aed2c8f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
